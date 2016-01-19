ad894553468f196e8eb19f62fad943a28ed141f6
Jakub Bogacz <j.bogacz@outlook.com>
2016-01-19 21:17:59 +0100

    Exercise 2: Mine

--- ExpressWorks/app.js
+++ ExpressWorks/app.js
@@ -1,8 +1,6 @@
 ﻿var express = require("express");
 var app = express();
 
-app.get("/home", function (req, res) {
-	res.end("Hello World!");
-});
+app.use(express.static(process.argv[3] || "public"));
 
 app.listen(process.argv[2]);
