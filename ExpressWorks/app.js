0771f6ee680ca50584fb8a200fddc662a4c7ff33
Jakub Bogacz <j.bogacz@outlook.com>
2016-01-23 11:01:45 +0100

    Exercise 4: Official

--- ExpressWorks/app.js
+++ ExpressWorks/app.js
@@ -1,25 +1,11 @@
-﻿var express = require("express");
-var app = express();
-var path = require("path");
-var bodyparser = require("body-parser");
+﻿var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
 
-app.set("views", process.argv[3] || "templates");
-app.set("view engine", "jade");
+app.use(bodyParser.urlencoded({extended: false}))
 
-app.use(bodyparser.urlencoded({ extended: false }));
+app.post('/form', function(req, res) {
+res.send(req.body.str.split('').reverse().join(''))
+})
 
-app.get("/home", function (req, res) {
-	res.render("index", { date: new Date().toDateString() });
-});
-
-app.get("/form", function (req, res) {
-	res.sendFile(path.join(__dirname + "/public/form.html"));
-});
-
-app.post("/form", function (req, res) {
-	var typed = req.body.str;
-	//res.render("form", { typed: typed });
-	res.end(typed.split("").reverse().join(""));
-});
-
-app.listen(process.argv[2]);
+app.listen(process.argv[2])
