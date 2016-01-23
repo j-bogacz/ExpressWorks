89ea54ddafd00a5c7c67ac91a5cc70763459363e
Jakub Bogacz <j.bogacz@outlook.com>
2016-01-23 16:20:52 +0100

    Exercise 7: Official

--- ExpressWorks/app.js
+++ ExpressWorks/app.js
@@ -1,11 +1,9 @@
 ﻿var express = require('express')
 var app = express()
-var crypto = require("crypto");
 
-app.put("/message/:id", function (req, res) {
-	var id = req.params.id;
-	var str = crypto.createHash("sha1").update(new Date().toDateString() + id).digest("hex");
-	res.end(str);
-});
+app.get('/search', function (req, res) {
+	var query = req.query
+	res.send(query)
+})
 
 app.listen(process.argv[2])
