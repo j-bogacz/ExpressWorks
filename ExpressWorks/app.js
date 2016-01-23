2d2a850c4d3a9b9783bce6053011a7819bd77cc6
Jakub Bogacz <j.bogacz@outlook.com>
2016-01-23 16:01:13 +0100

    Exercise 5: Mine

--- ExpressWorks/app.js
+++ ExpressWorks/app.js
@@ -1,21 +1,14 @@
 ﻿var express = require("express");
 var app = express();
 var path = require("path");
-var bodyparser = require("body-parser");
+var stylus = require("stylus");
 
-app.set("views", process.argv[3] || "templates");
-app.set("view engine", "jade");
+app.use(stylus.middleware(process.argv[3] || "public"));
 
-app.use(bodyparser.urlencoded({ extended: false }));
+app.use(express.static(process.argv[3] || "public"));
 
-app.get("/form", function (req, res) {
-	res.sendFile(path.join(__dirname + "/public/form.html"));
-});
-
-app.post("/form", function (req, res) {
-	var typed = req.body.input;
-	var typed_reversed = typed.split("").reverse().join("");
-	res.render("form", { typed: typed, typed_reversed: typed_reversed });
+app.get("/", function (req, res) {
+	res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
 app.listen(process.argv[2]);
