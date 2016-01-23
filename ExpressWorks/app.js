e022bcb0694b244a09655df1e9cf7da7c5d50277
Jakub Bogacz <j.bogacz@outlook.com>
2016-01-23 11:00:55 +0100

    Exercise 4: Mine

--- ExpressWorks/app.js
+++ ExpressWorks/app.js
@@ -1,12 +1,25 @@
 ﻿var express = require("express");
 var app = express();
+var path = require("path");
+var bodyparser = require("body-parser");
 
 app.set("views", process.argv[3] || "templates");
-
 app.set("view engine", "jade");
+
+app.use(bodyparser.urlencoded({ extended: false }));
 
 app.get("/home", function (req, res) {
 	res.render("index", { date: new Date().toDateString() });
+});
+
+app.get("/form", function (req, res) {
+	res.sendFile(path.join(__dirname + "/public/form.html"));
+});
+
+app.post("/form", function (req, res) {
+	var typed = req.body.str;
+	//res.render("form", { typed: typed });
+	res.end(typed.split("").reverse().join(""));
 });
 
 app.listen(process.argv[2]);
