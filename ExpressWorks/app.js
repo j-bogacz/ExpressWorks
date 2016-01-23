644029173957676a87a1e771cb88309e6e5b5208
Jakub Bogacz <j.bogacz@outlook.com>
2016-01-23 16:02:01 +0100

    Exercise 5: Official

--- ExpressWorks/app.js
+++ ExpressWorks/app.js
@@ -1,14 +1,8 @@
-﻿var express = require("express");
-var app = express();
-var path = require("path");
-var stylus = require("stylus");
+﻿var express = require('express')
+var app = express()
 
-app.use(stylus.middleware(process.argv[3] || "public"));
+app.use(require('stylus').middleware(process.argv[3]));
+app.use(express.static(process.argv[3]));
 
-app.use(express.static(process.argv[3] || "public"));
 
-app.get("/", function (req, res) {
-	res.sendFile(path.join(__dirname + "/public/index.html"));
-});
-
-app.listen(process.argv[2]);
+app.listen(process.argv[2])
