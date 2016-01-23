eb5a2a9cbb8c8341e49e8ae4bb9f766f34ecc502
Jakub Bogacz <j.bogacz@outlook.com>
2016-01-23 11:08:03 +0100

    Exercise 4: Mine (final)

--- ExpressWorks/app.js
+++ ExpressWorks/app.js
@@ -1,11 +1,21 @@
-﻿var express = require('express')
-var bodyParser = require('body-parser')
-var app = express()
+﻿var express = require("express");
+var app = express();
+var path = require("path");
+var bodyparser = require("body-parser");
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.set("views", process.argv[3] || "templates");
+app.set("view engine", "jade");
 
-app.post('/form', function(req, res) {
-res.send(req.body.str.split('').reverse().join(''))
-})
+app.use(bodyparser.urlencoded({ extended: false }));
 
-app.listen(process.argv[2])
+app.get("/form", function (req, res) {
+	res.sendFile(path.join(__dirname + "/public/form.html"));
+});
+
+app.post("/form", function (req, res) {
+	var typed = req.body.input;
+	var typed_reversed = typed.split("").reverse().join("");
+	res.render("form", { typed: typed, typed_reversed: typed_reversed });
+});
+
+app.listen(process.argv[2]);
