0461dd7ce6876d49de4f6347365c08da1a82230f
Jakub Bogacz <j.bogacz@outlook.com>
2016-01-23 16:30:57 +0100

    Exercise 8: Official

--- ExpressWorks/app.js
+++ ExpressWorks/app.js
@@ -1,11 +1,19 @@
 ﻿var express = require('express')
-var fs = require('fs')
 var app = express()
+var fs = require('fs')
 
 app.get('/books', function (req, res) {
-	fs.readFile(process.argv[3], function (err, data) {
-		res.json(JSON.parse(data));
-	});
+	var filename = process.argv[3]
+	fs.readFile(filename, function (e, data) {
+		if (e) return res.sendStatus(500)
+		try {
+			books = JSON.parse(data)
+		} catch (e) {
+			res.sendStatus(500)
+		}
+		res.json(books)
+	})
 })
 
 app.listen(process.argv[2])
+
