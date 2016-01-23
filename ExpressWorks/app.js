ae3659d5b9448c5206e113ac5b00280e5d66177f
Jakub Bogacz <j.bogacz@outlook.com>
2016-01-23 16:13:14 +0100

    Exercise 6: Mine

--- ExpressWorks/app.js
+++ ExpressWorks/app.js
@@ -1,8 +1,11 @@
 ﻿var express = require('express')
 var app = express()
+var crypto = require("crypto");
 
-app.use(require('stylus').middleware(process.argv[3]));
-app.use(express.static(process.argv[3]));
-
+app.put("/message/:id", function (req, res) {
+	var id = req.params.id;
+	var str = crypto.createHash("sha1").update(new Date().toDateString() + id).digest("hex");
+	res.end(str);
+});
 
 app.listen(process.argv[2])
